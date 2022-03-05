cfa127352375b08330a5f6a3472f9d966cbeb921
stop <djangosettings.test.mail@gmail.com>
2022-03-05 12:07:27 +0200

    added new email field

--- email_sender/views.py
+++ email_sender/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.conf import settings
 import pandas as pd
 import urllib
+import smtplib
 from django.core.mail import EmailMessage
 from django.contrib import messages
 
@@ -11,7 +12,12 @@ def homepage(request):
         try:
             SHEET_URL = request.POST['sheet_url']
             URL_EDITED = SHEET_URL.replace("/edit#gid=", "/export?format=csv&gid=")
-            data = pd.read_csv(URL_EDITED)  
+            data = pd.read_csv(URL_EDITED)
+            sender_email = request.POST['email']  
+            sender_password = request.POST['password']
+            if sender_email != '' and sender_password != '':
+                settings.EMAIL_HOST_USER = sender_email
+                settings.EMAIL_HOST_PASSWORD = sender_password                
             for email in data['email']:
                 email = email.replace(' ','')
                 print(email)
@@ -27,6 +33,8 @@ def homepage(request):
                 messages.error(request, f'Wrong-site URL. Must be the "docs.google.com/spreadsheets" table link! Message: {e}')
             elif isinstance(e, (pd.errors.ParserError,FileNotFoundError)):
                 messages.error(request, f'Incorrect site URL. Copy link from the URL address field in your web browser! Message: {e}')
+            elif isinstance(e, (smtplib.SMTPAuthenticationError)):
+                messages.error(request, f'Incorrect email settings (incorrect password, wrong or unconfigured email)! Message: {e}')
             else:
                 messages.error(request, 'Error. Something was wrong!')
             print(type(e))
