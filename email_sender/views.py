df05a375caadba607ca1fb65b941d779490c6e57
stop <djangosettings.test.mail@gmail.com>
2022-03-05 09:56:19 +0200

    Added error handlers

--- email_sender/views.py
+++ email_sender/views.py
@@ -1,22 +1,34 @@
 from django.shortcuts import render, get_object_or_404
 from django.conf import settings
 import pandas as pd
-
+import urllib
 from django.core.mail import EmailMessage
+from django.contrib import messages
 
 
 def homepage(request):
     if request.method == 'POST':
-        SHEET_URL = request.POST['sheet_url']
-        URL_EDITED = SHEET_URL.replace("/edit#gid=", "/export?format=csv&gid=")
-        data = pd.read_csv(URL_EDITED)  
-        for email in data['email']:
-            email = email.replace(' ','')
-            print(email)
-
-            subject = f'Subject: {request.POST.get("subject")}'
-            body = f'Message:\n{request.POST["message"]}'
-            email = EmailMessage(subject, body, to=[email])
-            email.send()
+        try:
+            SHEET_URL = request.POST['sheet_url']
+            URL_EDITED = SHEET_URL.replace("/edit#gid=", "/export?format=csv&gid=")
+            data = pd.read_csv(URL_EDITED)  
+            for email in data['email']:
+                email = email.replace(' ','')
+                print(email)
+                subject = f'Subject: {request.POST.get("subject")}'
+                body = f'Message:\n{request.POST["message"]}'
+                email = EmailMessage(subject, body, to=[email])
+                email.send()
+            messages.info(request, 'All messages was sended!')
+        except Exception as e:
+            if isinstance(e, KeyError):
+                messages.error(request, f'Incorrect file structure. Missed "email" title in cell A1! Message: {e}')
+            elif isinstance(e, (urllib.error.HTTPError,)):
+                messages.error(request, f'Wrong-site URL. Must be the "docs.google.com/spreadsheets" table link! Message: {e}')
+            elif isinstance(e, (pd.errors.ParserError,FileNotFoundError)):
+                messages.error(request, f'Incorrect site URL. Copy link from the URL address field in your web browser! Message: {e}')
+            else:
+                messages.error(request, 'Error. Something was wrong!')
+            print(type(e))
 
     return render(request, 'index.html')
